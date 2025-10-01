586ad2d3828fe69b2353b3ce86ab5ec12f8d61fe
Fatima Ezzahrae Koukou <fakoukou@c4r4p2.1337.ma>
2025-10-01 12:01:33 +0100

    d

--- cub3d.h
+++ cub3d.h
@@ -6,7 +6,7 @@
 /*   By: fakoukou <fakoukou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/11 10:49:39 by zait-err          #+#    #+#             */
-/*   Updated: 2025/09/29 10:46:07 by fakoukou         ###   ########.fr       */
+/*   Updated: 2025/10/01 12:00:39 by fakoukou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,40 +18,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include "get_next_line/get_next_line.h"
 #include "get_next_line/get_next_line.h"
 // #include "libft/libft.h"
 
-typedef struct s_textures {
-    char *NO;
-    char *SO;
-    char *EA;
-    char *WE;
-    char *S;
-} t_textures;
-
-
-int parse_color(char *line, int print_color);
-void check_texture_line(t_textures *tex, char *line);
-void check_path(char *path);
-int             parse_map(char *file_name);
-int             count_line_map(int fd);
-char            *read_map(int fd);
-int get_first_line_map(const char *line);
-char	*ft_strjoinn(char *s1, char *s2);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
-char	**ft_split(char const *s, char c);
 
 typedef struct s_player
 {
-    int x;          // player row (map index)
-    int y;          // player col (map index)
+    double x;          // player row (map index)
+    double y;          // player col (map index)
     char dir;       // 'N', 'S', 'E', 'W'
+    double dir_x; // direction vector (unit vector) x
+    double dir_y; // dir vector y
+    double plane_x; // camera plane vector (perpendicular to dir) x
+    double plane_y; //camera plane vector y
 }   t_player;
 
 typedef struct s_map
@@ -62,14 +41,19 @@ typedef struct s_map
     t_player player;    // player position & direction
 }   t_map;
 
+typedef struct s_color
+{
+    int r;
+    int g;
+    int b;
+}t_color;
+
 typedef struct s_config
 {
     char    *no_tex;    // path to North texture
     char    *so_tex;    // South
     char    *we_tex;    // West
     char    *ea_tex;    // East
-    int     floor[3];   // RGB values
-    int     ceil[3];    // RGB values
     t_map   map;        // the map info
 }   t_config;
 
@@ -85,14 +69,15 @@ int             find_big_line(char **map);
 char            **square_map(char **map, int len);
 size_t	        ft_strlcpy(char *dest, const char *src, size_t dstsize);
 void            trim_newline(char **map);
-void            valid_map(char **full_map);
-int             check_top_border(char **map, int cols);
-int             check_bottom_border(char **map, int rows, int cols);
-int             check_left_right_border(char **map, int rows, int cols);
-int             check_inside(char **map, int rows, int cols);
-int             check_inside_2(char **map, int rows, int cols);
-int             check_player_pos(char **map, int rows, int cols);
-int             check_space_map(char **map, int rows, int cols);
+void            valid_map(t_map *map);
+int             check_top_border(t_map *map);
+int             check_bottom_border(t_map *map);
+int             check_left_right_border(t_map *map);
+int             check_inside(t_map *map);
+int             check_inside_2(t_map *map);
+int             check_player_pos(t_map *map);
+int             check_space_map(t_map *map);
 void            print_valid();
-int white_spaces(char *line, int i);
+t_map *init_map();
+
 #endif
